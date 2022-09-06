78224bd70b30fbe53e15f35e3ff27253cdd74325
SaganJackson <sagan.jacksonmoses@gmail.com>
2022-09-06 15:08:43 -0700

    Completed Lab 3.4

--- Lab02.2/src/Television.java
+++ Lab02.2/src/Television.java
@@ -1,15 +1,35 @@
 public class Television {
 
+  public static final int MIN_VOLUME = 0;
+  public static final int MAX_VOLUME = 100;
+
+  private static int instanceCount = 0;
+
   private String brand; //field
   private int volume;
 
-//constructors are located after the fields and before the methods
-  /* public Television(String brand){
-    this.brand = brand;
-  }*/
+
+  //constructors are located after the fields and before the methods
+  public Television() {
+    instanceCount++;
+  }
+
+  public Television(String brand) {
+    this();
+    setBrand(brand);
+  }
+
+  public Television(String brand, int volume) {
+    //this.brand = brand;
+    this(brand);
+    setVolume(volume);
+
+  }
+
+
   //business methods first and then setters and getters
-  public void turnOn(){
-    // TODO: 9/1/2022
+  public void turnOn() {
+
     boolean isConnected = verifyInternetConnection();
     //sout = System.out.println();
     System.out.println("The " + brand + " television is on, with a volume of " + volume);
@@ -19,6 +39,11 @@ public class Television {
 
     System.out.println("The " + brand + " television is off");
   }
+
+  public static int getInstanceCount() {
+    return instanceCount;
+  }
+
 
   public String getBrand() {
     return brand;
@@ -33,14 +58,20 @@ public class Television {
   }
 
   public void setVolume(int volume) {
-    this.volume = volume;
+
+    if (volume < MIN_VOLUME || volume > MAX_VOLUME) {
+      System.out.printf("%d is invalid; volume must be between %d and %d (inclusive).%n", volume,
+          MIN_VOLUME, MAX_VOLUME);
+    } else {
+      this.volume = volume;
+    }
   }
 
   private boolean verifyInternetConnection() {
     return true;
   }
 
-  public String toString(){
+  public String toString() {
     return "Television: brand=" + brand + ", volume=" + volume;
   }
 }
--- Lab02.2/src/TelevisionClient.java
+++ Lab02.2/src/TelevisionClient.java
@@ -4,22 +4,34 @@ public class TelevisionClient {
 
     Television tv1 = new Television();
     tv1.setBrand("Samsung");
-    tv1.setVolume(25);
-
-    Television tv2 = new Television();
-    tv2.setBrand("Sony");
-    tv2.setVolume(32);
+    tv1.setVolume(125);
 
 
     tv1.turnOn();
     tv1.turnOff();
 
+    System.out.println(tv1);
+    System.out.printf("%d television instances created", Television.getInstanceCount());
+
+    Television tv2 = new Television("Sony", 50);
+    //tv2.setBrand("Sony");
+    //tv2.setVolume(32);
+
     tv2.turnOn();
     tv2.turnOff();
+    System.out.printf("%d television instances created", Television.getInstanceCount());
+    System.out.println(tv2);
+
+    Television tv3 = new Television("LG");
+
+    tv3.turnOn();
+    tv3.turnOff();
 
     //System.out.println(tv1.toString());
-    System.out.println(tv1);
-    System.out.println(tv2);
+
+
+    System.out.println(tv3);
+    System.out.printf("%d television instances created", Television.getInstanceCount());
   }
 
 }
