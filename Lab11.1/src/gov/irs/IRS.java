0a87560045eec5e94233a65255f71f4698039693
SaganJackson <sagan.jacksonmoses@gmail.com>
2022-09-16 12:47:49 -0700

    Completed Lab 11.1

---
+++ Lab11.1/src/gov/irs/IRS.java
@@ -0,0 +1,41 @@
+/*
+ * This code is sample code, provided as-is, and we make no
+ * warranties as to its correctness or suitability for any purpose.
+ *
+ * We hope that it's useful to you.  Enjoy.
+ * Copyright LearningPatterns Inc.
+ */
+ 
+package gov.irs;
+
+import com.hr.personnel.Employee;
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * The IRS maintains a collection of TaxPayers and collects taxes from them.
+ *
+ * Note the interface type TaxPayer, used throughout the class.
+ */
+public class IRS {
+    // what types of objects can this array contain?
+    //private TaxPayer[] payers = new TaxPayer[100];
+    private final Collection<TaxPayer> payers = new ArrayList<>();
+
+
+    public void collectTaxes() {
+        for (TaxPayer payer : payers) {
+            double deduction = payer.getStandardDeduction();
+            //System.out.println("Tax payer's deduction is: " + deduction);
+            System.out.printf("Taxpayer's deduction is: %,.2f%n", deduction);
+            payer.payTaxes();
+            System.out.println();
+        }
+    }
+    
+    // helper method to add a TaxPayer to the array
+    // what types of objects can be passed to this method?
+    public void register(TaxPayer payer) {
+        payers.add(payer);
+    }
+}
